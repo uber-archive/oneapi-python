932961e0b5d53b59c0c2e1143c93959b09496705
Mike Kadin <kadin@uber.com>
2016-10-04 18:19:10 -0700

    decrease timeout

--- oneapi/http.py
+++ oneapi/http.py
@@ -82,7 +82,7 @@ def execute_request(method, url, data=None, headers=None, data_format=None):
             request.add_header(key, value)
 
     try:
-        url = opener.open(request, data=body, timeout=5)
+        url = opener.open(request, data=body, timeout=2)
 
         http_code = url.getcode()
         headers = headers
